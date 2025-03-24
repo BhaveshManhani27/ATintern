529a068f27b367c43ea4558bea0494174c154414
BhaveshManhani27 <bhaveshmanhani62@gmail.com>
2025-03-24 15:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,17 +11,4 @@ def analyze(company_name: str = Query(..., description="Enter the company name (
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-=======
-from fastapi import FastAPI, Query
-from api import analyze_news
 
-app = FastAPI()
-
-@app.get("/analyze")
-def analyze(company_name: str = Query(..., description="Enter the company name (e.g., Tesla, Apple)")):
-    return analyze_news(company_name)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
->>>>>>> 37924f66cc4f27fa0c6c2f1c909690f790c12843
